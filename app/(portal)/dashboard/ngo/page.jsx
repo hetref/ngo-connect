73f085f43ab7ef485f3cc9d62ddc530c474790c0
ayushijadhav2006 <124800578+ayushijadhav2006@users.noreply.github.com>
2025-01-28 21:33:36 +0530

    deleted sidebar

--- app/(portal)/dashboard/ngo/page.jsx
+++ app/(portal)/dashboard/ngo/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const NGODashboardPage = () => {
-  return <div>NGODashboardPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold">NGO Dashboard</h1>
+    </div>
+  );
 };
 
 export default NGODashboardPage;

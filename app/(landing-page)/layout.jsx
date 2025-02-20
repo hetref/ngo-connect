c259a7ad8d9938a5e6af3be23c87d93fcb63268c
Aryan shinde <shindearyan179@gmail.com>
2025-02-20 23:04:22 +0530

    Added Footer in pages, and added logo's variation

--- app/(landing-page)/layout.jsx
+++ app/(landing-page)/layout.jsx
@@ -1,10 +1,12 @@
 import FloatingNavbar from "@/components/floating-navbar";
+import { Footer } from "@/components/footer";
 import React from "react";
 
 const Layout = ({ children }) => (
   <div>
     <FloatingNavbar />
     {children}
+    <Footer />
   </div>
 );
 
--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,6 @@ export default function RootLayout({ children }) {
             <main className="flex-1">
               {children}
             </main>
-            <Footer />
           </div>
         </ThemeProvider>
       </body>
